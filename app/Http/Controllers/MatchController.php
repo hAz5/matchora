3741e7c288460b3c6a35d80b1a5a8985115ec1d5
tresa02 <claymore0465@gmail.com>
2018-08-25 17:47:09 +0430

    add today match

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Match;
 use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
-    //
+
+    public function todayMatch()
+    {
+        $matches = Match::getNotDone()->todayMatch()->paginate(20);
+
+        return view('');
+    }
 }
