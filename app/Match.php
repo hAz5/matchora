7be655278cb9a26c1f77c2fcf2a6c59c5aac34cb
tresa02 <claymore0465@gmail.com>
2018-08-26 16:05:20 +0430

    generate ide helper

--- app/Match.php
+++ app/Match.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Match getNotDone()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Match getOK()
  * @mixin \Eloquent
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Match todayMatch()
  */
 class Match extends Model
 {
